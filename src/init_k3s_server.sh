cf250f50c9cc8e4d9c3a4f01bcaf5d6e81aeca99
MakFil <makfil1177@gmail.com>
2024-11-03 13:47:35 +0100

    Add k3s API server check

--- src/init_k3s_server.sh
+++ src/init_k3s_server.sh
@@ -13,18 +13,37 @@ curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --disable tra
 
 # Wait for k3s to start
 echo "Waiting for k3s to start..."
-for i in {1..30}; do
-    if kubectl get nodes &> /dev/null; then
+for i in {1..40}; do
+    if systemctl is-active --quiet k3s; then
         echo "k3s is up and running!"
         break
     fi
     echo "Waiting for k3s to be ready... (attempt $i)"
-    sleep 2
+    sleep 5
 done
 
 # Check if k3s started successfully
-if ! sudo systemctl is-active --quiet k3s; then
+if ! systemctl is-active --quiet k3s; then
     echo "k3s failed to start. Exiting..."
+    exit 1
+fi
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+# Check if k3s server is ready
+echo "Waiting for k3s API server to be ready..."
+for i in {1..40}; do
+    if kubectl get nodes &> /dev/null; then
+        echo "k3s API server is ready"
+        break
+    fi
+    echo "Waiting for k3s API server to be ready... (attempt $i)"
+    sleep 5
+done
+
+# Check if k3s server is still not ready
+if ! kubectl get nodes &> /dev/null; then
+    echo "k3s API server failed to start. Exiting..."
     exit 1
 fi
 
