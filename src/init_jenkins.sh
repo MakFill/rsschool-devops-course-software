6409c56020394ce2306d288897b05742cf0182c3
MakFil <makfil1177@gmail.com>
2024-11-03 22:41:53 +0100

    Verbose test logs

--- src/init_jenkins.sh
+++ src/init_jenkins.sh
@@ -60,6 +60,14 @@ while true; do
     name=$(echo $status | jq -r '.name')
     state=$(echo $status | jq -r '.state')
     echo "Init Container: $name, Status: $state"
+
+    if [[ "$state" == "terminated" ]]; then
+      exit_code=$(echo $status | jq -r '.state.terminated.exitCode')
+      if [[ "$exit_code" -ne 0 ]]; then
+        echo "Init Container: $name has failed with exit code $exit_code. Fetching logs..."
+        kubectl logs $POD_NAME -n jenkins -c $name
+      fi
+    fi
   done
   sleep 5  
 done
